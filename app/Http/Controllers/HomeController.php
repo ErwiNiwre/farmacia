0f23a1909bc85b71c8d4adba04fe2fad50b3f0a9
veimarbc <veimarbc@gmail.com>
2025-07-02 14:22:54 -0400

    dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Models\Producto;
+use Illuminate\Support\Facades\DB;
+use App\Models\CompraDetalle;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
@@ -21,16 +25,144 @@ class HomeController extends Controller
             $session_name = $session_auth->nombre;
         }
 
-        // $patients = Patient::All();
-        // $employees = Staff::All()->slice(1);
+      $medicamentos = DB::table('productos')
+            ->select(
+                'productos.id',
+                'producto',
+                'generico',
+                'concentracion',
+                'marca',
+                'presentacion',
+                'accion_terapeutica',
+                'unidad_medida',
+                'estado',
+                'compra_detalles.cantidad',
+                'compra_detalles.precio_unitario',
+                'precio_venta',
+                'vencimiento',
+                'compra_detalles.created_at as fecha_compra'
+            )
+            ->join('concentraciones', 'concentraciones.id', '=', 'productos.concentracion_id')
+            ->join('marcas', 'marcas.id', '=', 'productos.marca_id')
+            ->join('presentaciones', 'presentaciones.id', '=', 'productos.presentacion_id')
+            ->join('accion_terapeuticas', 'accion_terapeuticas.id', '=', 'productos.accion_terapeutica_id')
+            ->join('unidad_medidas', 'unidad_medidas.id', '=', 'productos.unidad_medida_id')
+            ->join('compra_detalles', 'compra_detalles.producto_id', '=', 'productos.id')
+            ->where('tipo_producto', 'M')
+            //->whereDate('compra_detalles.vencimiento', Carbon::now()->addDays(40)->toDateString())
+            ->whereBetween('compra_detalles.vencimiento', [
+            Carbon::now()->toDateString(),
+            Carbon::now()->addDays(40)->toDateString()
+            ])
+            // ->whereDate('compra_detalles.vencimiento', '=', Carbon::now()->addDays(60)->toDateString())
+            ->whereNull('compra_detalles.deleted_at')
+            ->whereNull('productos.deleted_at')
+            ->get();
+// print_r($medicamentos);
+// exit;
+        $insumos = DB::table('productos')
+            ->select(
+                'productos.id',
+                'producto',
+                'generico',
+                'concentracion',
+                'marca',
+                'presentacion',
+                'accion_terapeutica',
+                'unidad_medida',
+                'estado',
+                'compra_detalles.cantidad',
+                'compra_detalles.precio_unitario',
+                'precio_venta',
+                'vencimiento',
+                'compra_detalles.created_at as fecha_compra'
+            )
+            ->join('concentraciones', 'concentraciones.id', '=', 'productos.concentracion_id')
+            ->join('marcas', 'marcas.id', '=', 'productos.marca_id')
+            ->join('presentaciones', 'presentaciones.id', '=', 'productos.presentacion_id')
+            ->join('accion_terapeuticas', 'accion_terapeuticas.id', '=', 'productos.accion_terapeutica_id')
+            ->join('unidad_medidas', 'unidad_medidas.id', '=', 'productos.unidad_medida_id')
+            ->join('compra_detalles', 'compra_detalles.producto_id', '=', 'productos.id')           
+            ->where('tipo_producto', 'I')
+            ->whereBetween('compra_detalles.vencimiento', [
+            Carbon::now()->toDateString(),
+            Carbon::now()->addDays(40)->toDateString()
+            ])
+            
+            ->whereNull('compra_detalles.deleted_at')
+            ->whereNull('productos.deleted_at')
+            ->get();
 
         return view(
             'home.index',
             compact(
                 'session_auth',
-                'session_name'
-                // 'patients',
-                // 'employees'
+                'session_name',
+                'medicamentos',
+                'insumos'
+            )
+        );
+    }
+
+         public function farmacia()
+    {
+      
+        $medicamentos = DB::table('productos')
+            ->select(
+                'productos.id',
+                'producto',
+                'generico',
+                'concentracion',
+                'marca',
+                'presentacion',
+                'accion_terapeutica',
+                'unidad_medida',
+                'estado',
+                'cantidad',
+                'precio_venta',
+                'vencimiento',
+                'created_at as fecha_compra'
+            )
+            ->join('concentraciones', 'concentraciones.id', '=', 'productos.concentracion_id')
+            ->join('marcas', 'marcas.id', '=', 'productos.marca_id')
+            ->join('presentaciones', 'presentaciones.id', '=', 'productos.presentacion_id')
+            ->join('accion_terapeuticas', 'accion_terapeuticas.id', '=', 'productos.accion_terapeutica_id')
+            ->join('unidad_medidas', 'unidad_medidas.id', '=', 'productos.unidad_medida_id')
+            ->where('tipo_producto', 'M')
+            ->whereNull('productos.deleted_at')
+            ->get();
+print_r($medicamentos);
+exit;
+        $insumos = DB::table('productos')
+            ->select(
+                'productos.id',
+                'producto',
+                'generico',
+                'concentracion',
+                'marca',
+                'presentacion',
+                'accion_terapeutica',
+                'unidad_medida',
+                'estado',
+                'cantidad',
+                'precio_venta'
+            )
+            ->join('concentraciones', 'concentraciones.id', '=', 'productos.concentracion_id')
+            ->join('marcas', 'marcas.id', '=', 'productos.marca_id')
+            ->join('presentaciones', 'presentaciones.id', '=', 'productos.presentacion_id')
+            ->join('accion_terapeuticas', 'accion_terapeuticas.id', '=', 'productos.accion_terapeutica_id')
+            ->join('unidad_medidas', 'unidad_medidas.id', '=', 'productos.unidad_medida_id')
+            ->join('compra_detalles', 'compra_detalles.producto_id', '=', 'productos.id')
+            ->where('tipo_producto', 'I')
+            ->whereDate('compra_detalles.fecha_vencimiento', Carbon::now()->addDays(40)->toDateString())
+            ->whereNull('productos.deleted_at')
+            ->get();
+
+        return view(
+            'Homer.index',
+            compact(
+                'medicamentos',
+                'insumos'
             )
         );
     }
