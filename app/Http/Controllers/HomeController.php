bfad8a0f6f3ba25d7aa8cc6365d493d338ef7bc5
veimarbc <veimarbc@gmail.com>
2025-10-07 13:16:54 -0400

    modificacion en home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -79,7 +79,7 @@ class HomeController extends Controller
                 'accion_terapeutica',
                 'unidad_medida',
                 'estado',
-                'compra_detalles.cantidad',
+                'compra_detalles.cantidad_total as cantidad',
                 'compra_detalles.precio_unitario',
                 'precio_venta',
                 'vencimiento',
@@ -92,6 +92,7 @@ class HomeController extends Controller
             ->join('unidad_medidas', 'unidad_medidas.id', '=', 'productos.unidad_medida_id')
             ->join('compra_detalles', 'compra_detalles.producto_id', '=', 'productos.id')
             ->where('tipo_producto', 'I')
+            ->where('compra_detalles.cantidad_total', '<>', 0)
             ->whereBetween('compra_detalles.vencimiento', [
                 Carbon::now()->toDateString(),
                 Carbon::now()->addDays(40)->toDateString()
